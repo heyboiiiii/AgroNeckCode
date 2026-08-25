97c157d1d4846240bea5325a8b077a1eac3650d0
BraulioRom20 <braulioemilioromero7@gmail.com>
2026-08-25 11:04:39 -0300

    refactor: modificacion en el switcheo de mosfets

--- main/main.c
+++ main/main.c
@@ -3,6 +3,7 @@
 #include <sys/time.h>
 #include "esp_log.h"    
 #include "esp_sleep.h"
+#include "esp_system.h"
 #include "esp_adc/adc_oneshot.h" 
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
@@ -29,8 +30,9 @@
 // Variable que guarda el estado de mosfets aun en Deep Sleep
 RTC_DATA_ATTR bool estado_mosfet = true; //true = estado inicial, 2+3 y false = estado secundario, 1+4
 // Conmutación de baterías
-#define TIEMPO_CAMBIO_BATS 86400ULL // Constante que expresa 24hs en microsegs
+#define TIEMPO_CAMBIO_BATS 10ULL 
 RTC_DATA_ATTR uint64_t tiempo_switch = 0;
+RTC_DATA_ATTR bool sistema_inicializado = false;
 
 
 // VARIABLES DE DATOS
@@ -46,6 +48,7 @@ float temp_interna; // temperatura final del calculo
 
 // Inicialización de Salida de MOSFETs
 void init_mosfet_gpios() {
+    gpio_deep_sleep_hold_dis();
     gpio_hold_dis(MOSFET1);
     gpio_hold_dis(MOSFET2);
     gpio_hold_dis(MOSFET3);
@@ -54,6 +57,8 @@ void init_mosfet_gpios() {
         .intr_type = GPIO_INTR_DISABLE,           // Deshabilitar interrupciones
         .mode = GPIO_MODE_OUTPUT,                 // Configurar como salida
         .pin_bit_mask = GPIO_MOSFET_MASK,         // Uso la mascara
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
     };
     gpio_config(&io_config);
 }
@@ -81,23 +86,35 @@ void switch_mosfet() {
     aplica_estado_mosfet();
 }
 
-
-void hora_actual() {
+uint64_t tiempo_medido(void) {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    uint64_t tiempo_actual = (uint64_t)tv.tv_sec;
+    return (uint64_t)tv.tv_sec;
+}
 
-    if (tiempo_switch == 0) {
+
+void hora_actual() {
+    uint64_t tiempo_actual = tiempo_medido();
+
+    // Si es el primer encendido físico del equipo
+    if (!sistema_inicializado) {
         tiempo_switch = tiempo_actual;
+        sistema_inicializado = true;
         aplica_estado_mosfet();
+        ESP_LOGI("MAIN", "Tiempo base: %llu", tiempo_switch);
         return;
     }
 
-    if ((tiempo_actual - tiempo_switch) >= TIEMPO_CAMBIO_BATS) {
+    uint64_t transcurrido = tiempo_actual - tiempo_switch;
+    ESP_LOGI("MOSFETS", "Tiempo actual: %llu s, ultimo switch: %llu, transcurrido: %llu s", 
+             tiempo_actual, tiempo_switch, transcurrido);
+
+    // Evalúa si ya pasó el tiempo necesario para cambiar de batería
+    if (transcurrido >= TIEMPO_CAMBIO_BATS) {
         switch_mosfet();
-        tiempo_switch = tiempo_actual;
-    }
-     else {
+        tiempo_switch = tiempo_actual; // Se actualiza el tiempo base con la hora actual
+    } else {
+        ESP_LOGI("MOSFETS", "Aún no transcurrió el tiempo. Se mantiene estado actual.");
         aplica_estado_mosfet();
     }
 }
@@ -235,6 +252,7 @@ void app_main(void)
         ESP_LOGI("MAIN_fwu","Inicio y congelo los mosfets.");
         //Configuro MOSFETs antes de entrar en Deep Sleep
         init_mosfet_gpios();
+        hora_actual();
         aplica_estado_mosfet();
         gpio_hold_en(MOSFET1);
         gpio_hold_en(MOSFET2);
@@ -244,7 +262,7 @@ void app_main(void)
         //Configuración de GPIO que despierte a la ESP32
         ESP_LOGI("MAIN_fwu","Preparo interrupción.");
         esp_sleep_enable_ext0_wakeup(WAKEUP_GPIO, WAKEUP_LEVEL); 
-
+        
         ESP_LOGI("MAIN_fwu","Entrando en deep sleep...");
         vTaskDelay(pdMS_TO_TICKS(100));
         
